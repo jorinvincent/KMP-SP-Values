b1d193ed154a0de4472f2f94e2ba4715de2f5018
Jorin Vincent <vincentj@umd.edu>
2022-02-17 21:09:11 -0500

    Updated files

---
+++ compute_values.py
@@ -0,0 +1,33 @@
+
+reader = open("input", "r")
+writer = open("output", "w")
+
+garbage = reader.readline()
+textlist = reader.readlines()
+text = ""
+for i in range(0, len(textlist)):
+    text = text + textlist[i].strip()
+
+sp = [0] * len(text)
+count = 0
+
+for i in range (1, len(text)):
+    for j in range (i, len(text)):
+        if (text[i:j + 1] == text[0:j - i + 1]):
+            if (sp[i + count] == 0):
+                sp[i + count] = count + 1
+            count = count + 1
+        else:
+            count = 0
+            break
+
+msg = ""
+
+for i in range(0, len(sp)):
+    msg = msg + str(sp[i]) + " "
+
+msg = msg[:-1]
+writer.write(msg)
+
+reader.close()
+writer.close()
